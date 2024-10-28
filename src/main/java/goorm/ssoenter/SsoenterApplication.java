cc328fb43e87fb559b3dbc5bbcb1a073203a73f4
haeun1107 <haeun9394@gmail.com>
2024-10-28 14:43:18 +0900

    Init: 프로젝트 초기 세팅

---
+++ src/main/java/goorm/ssoenter/SsoenterApplication.java
@@ -0,0 +1,13 @@
+package goorm.ssoenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsoenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsoenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/goorm/ssoenter/SsoenterApplicationTests.java
@@ -0,0 +1,13 @@
+package goorm.ssoenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsoenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
