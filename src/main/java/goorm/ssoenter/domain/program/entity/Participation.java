0c877bcda06b5fef694077ba7a983065d00753c8
haeun1107 <haeun9394@gmail.com>
2024-10-31 15:19:02 +0900

    Feat: participation entity 수정

--- src/main/java/goorm/ssoenter/domain/program/entity/Participation.java
+++ src/main/java/goorm/ssoenter/domain/program/entity/Participation.java
@@ -16,7 +16,7 @@ public class Participation {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long participationId;
 
     @ManyToOne
     @JoinColumn(name = "userId", nullable = false)
